26e6f20b11ddcaf013a033d0c2987e5b7edc01ff
Jeff Lu <ljeff@us.ibm.com>
2024-12-10 16:15:45 -0500

    revert to use trap

--- watch-container-stop.sh
+++ watch-container-stop.sh
@@ -1,23 +1,13 @@
 #!/bin/bash
 
-# Obtain the container ID of the current (open-horizon-bee-stack) container
-OHBS_CONTAINER_ID=$(cat /proc/self/cgroup | grep 'docker' | sed 's/.*\///')
+# Function to handle stop signal (SIGTERM)
+on_exit() {
+  echo "OHBS container stopping... executing stop script."
+  /app/bee-stack/bee-stack.sh stop
+  sleep 45
+}
+# Trap SIGTERM signal and execute `on_exit`
+trap on_exit SIGTERM
+# Start the main process (e.g., your app)
+echo "Starting main container process..."
 
-# Monitor OHBS status
-docker events --filter 'event=stop' | while read event
-do
-    # Extract the container name from the event
-    STOPPED_CONTAINER=$(echo "$event" | grep -oP 'container:\K\S+')
-
-    # Check if the stopped container is the parent container
-    if [[ "$STOPPED_CONTAINER" == "$HBS_CONTAINER_ID" ]]; then
-      echo "Parent container has been stopped. Shutting down related containers..."
-      /app/bee-stack/bee-stack.sh stop
-      sleep 45
-      break
-    fi
-done
-
-echo "Shutting down OHBS container."
-sleep 10
-exit 0
